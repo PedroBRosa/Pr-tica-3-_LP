35f0ab0ab1058124dc5fdfb4c95af3f3e5456201
Pedro Rosa <92191060+PedroBRosa@users.noreply.github.com>
2022-03-30 15:52:47 -0300

    Estruturando UI

--- src/Ex1/ValidadorCpf.java
+++ src/Ex1/ValidadorCpf.java
@@ -1,4 +1,12 @@
 package Ex1;
 
+import javax.swing.*;
+
 public class ValidadorCpf {
+    private JFormattedTextField nomeFormattedTextField;
+    private JFormattedTextField cpfFormattedTextField;
+    private JButton conferirCpfButton;
+    private JLabel nomeLabel;
+    private JLabel CPFLabel;
+    
 }
